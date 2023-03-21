5f2e7074c9c7514e5e50c2052a9df81b1593cff2
TangJetsada123 <jetsada_jinaka@hotmail.com>
2023-03-21 15:07:53 +0700

    fix: paht oauth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,12 +20,12 @@ export class AuthenController {
 
     @Get('')
     async find(@Query() code,@Res() res){
-        return res.redirect(`http://localhost:4000/auth/code?code=${code.code}`)
+        return res.redirect(`https://cmu-acad-backend-production.up.railway.app/auth/code?code=${code.code}`)
     }
 
     @Get('/code')
     async accessToken(@Query() code,@Res() res){
-        return this.httpService.post(`https://oauth.cmu.ac.th/v1/GetToken.aspx?code=${code.code}&redirect_uri=http://localhost:4000/auth/code&client_id=MgtZS8S3J9cAhGAUGhbdX9qFHR2mCySSG7pNHbW8&client_secret=CrJbXxZyb2b5YBhM3YsbfEAkux4ktYkExdNFBpUk&grant_type=authorization_code`).pipe(
+        return this.httpService.post(`https://oauth.cmu.ac.th/v1/GetToken.aspx?code=${code.code}&redirect_uri=https://cmu-acad-backend-production.up.railway.app/auth/code&client_id=MgtZS8S3J9cAhGAUGhbdX9qFHR2mCySSG7pNHbW8&client_secret=CrJbXxZyb2b5YBhM3YsbfEAkux4ktYkExdNFBpUk&grant_type=authorization_code`).pipe(
             map(response => res.redirect(`https://cmu-acad-backend-production.up.railway.app/auth/login?token=${response.data.access_token}`)))
     }
     
@@ -39,8 +39,8 @@ export class AuthenController {
                 }
             }
             ).toPromise();
-            // res.redirect(`https://cmu-acad.netlify.app/token=${token.token}`);
-            res.redirect(`http://localhost:3000/token=${token.token}`)
+            res.redirect(`https://cmu-acad.netlify.app/token=${token.token}`);
+            // res.redirect(`http://localhost:3000/token=${token.token}`)
             return this.userService.saveData(response.data)
             
         } catch (error) {
