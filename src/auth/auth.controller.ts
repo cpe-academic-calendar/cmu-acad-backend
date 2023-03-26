3c35758f6ed78c65bdd392f9772a4e7d250d44d9
TangJetsada123 <jetsada_jinaka@hotmail.com>
2023-03-26 14:16:56 +0700

    fix: path

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,13 +19,13 @@ export class AuthenController {
 
     @Get('')
     async find(@Query() code, @Res() res) {
-        return await res.redirect(`${process.env.localhost}/auth/code?code=${code.code}`)
+        return await res.redirect(`https://cmu-acad.netlify.app/auth/code?code=${code.code}`)
     }
 
     @Get('/code')
     async accessToken(@Query() code, @Res() res) {
-        return this.httpService.post(`https://oauth.cmu.ac.th/v1/GetToken.aspx?code=${code.code}&redirect_uri=${process.env.localhost}/auth/code&client_id=MgtZS8S3J9cAhGAUGhbdX9qFHR2mCySSG7pNHbW8&client_secret=CrJbXxZyb2b5YBhM3YsbfEAkux4ktYkExdNFBpUk&grant_type=authorization_code`).pipe(
-            map(response => res.redirect(`${process.env.localhost}/auth/login?token=${response.data.access_token}`)))
+        return this.httpService.post(`https://oauth.cmu.ac.th/v1/GetToken.aspx?code=${code.code}&redirect_uri=https://cmu-acad.netlify.app/auth/code&client_id=MgtZS8S3J9cAhGAUGhbdX9qFHR2mCySSG7pNHbW8&client_secret=CrJbXxZyb2b5YBhM3YsbfEAkux4ktYkExdNFBpUk&grant_type=authorization_code`).pipe(
+            map(response => res.redirect(`https://cmu-acad.netlify.app/auth/login?token=${response.data.access_token}`)))
     }
 
     @Get('/login')
